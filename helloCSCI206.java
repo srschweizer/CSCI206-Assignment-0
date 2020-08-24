8f554d4cb6b59eff087526bc8419038c6e74e601
srschweizer <69734683+srschweizer@users.noreply.github.com>
2020-08-23 21:10:37 -0600

    Add files via upload

---
+++ helloCSCI206.java
@@ -0,0 +1,12 @@
+/*
+Author: Scott Schweizer
+Course: CSCI-206
+Assignment: 0
+*/
+
+public class helloCSCI206 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello CSCI 206 World");
+    }
+}
